b227b61926f8c2f146e413c64b147b388dabeaef
RuiWangETH <carminasonata@gmail.com>
2023-10-03 15:13:43 +0200

    version 1.0

---
+++ requirements.sh
@@ -0,0 +1,12 @@
+pip install tensorboardX numpy>=1.16.2 torchgeometry>=0.1.2 pillow tqdm Ninja trimesh pyrender PyYAML loguru omegaconf
+pip install git+https://github.com/MPI-IS/mesh.git
+pip install git+https://github.com/otaheri/chamfer_distance
+pip install git+https://github.com/otaheri/bps_torch
+pip install pybullet
+pip install sk-video
+pip install rtree
+git clone https://github.com/vchoutas/torch-trust-ncg.git
+cd torch-trust-ncg
+python setup.py install
+pip install einops
+
